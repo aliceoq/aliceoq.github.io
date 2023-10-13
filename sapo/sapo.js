b5125bd3dedb37a0d94b6710c870dc4d56cc3777
aliceoq <aoqb@cin.ufpe.br>
2023-10-13 13:54:10 -0300

    feat: create react app

--- sapo/sapo.js
+++
@@ -1,5 +0,0 @@
-function decide() {
-  let option = Math.floor(Math.random() * 2);
-  let result = document.getElementById(`option-${option}`).value;
-  document.getElementById('result').innerHTML = result;
-}
---
+++ src/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+export { AppContainer, Header, AppLink };
